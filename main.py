c33e9625302abb8c7dbab84da480ad79cd745bc7
JoaoEnrique <joaoenriquealves13@gmail.com>
2025-09-16 12:58:44 -0300

    test: do watson

--- main.py
+++ main.py
@@ -42,9 +42,9 @@ async def process_file(file_input: UploadFile = File(...)):
 
         prompt = f"{EXAMPLES}\n{prompt_input}\nOutput:"
 
-        # watson_result = model.generate_text(prompt=prompt, guardrails=False)
-        # summary = watson_result.strip()
-        summary = "oii"
+        watson_result = model.generate_text(prompt=prompt, guardrails=False, timeout=120)
+        summary = watson_result.strip()
+        # summary = "oii"
 
 
         # Filtra vulnerabilidades confirmadas pelo Watson
